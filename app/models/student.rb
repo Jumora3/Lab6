4a2b95eca0d55d27458952f6804cea97be622638
Jun Mora <jumora@gbox.adnu.edu.ph>
2023-05-10 11:29:14 +0800

    Student Avatar

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,13 @@
 class Student < ApplicationRecord
+  def acceptable_image
+    return unless avatar.attached?
+
+    unless avatar.blob.byte_size<=1.megabyte
+      errors.add(:avatar," is too big")
+    end
+
+    acceptable_types=["image/jpeg","image/png"]
+    unless acceptable_types.include?(avatar.content_type)
+      errors.add(:avatar," must be a JPEG or PNG")
+    end
 end
